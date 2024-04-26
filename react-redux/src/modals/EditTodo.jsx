3df359e914aa3c8a63bc0ec50d0c3d00f161aaf0
Yavuz Tanrıverdi <sternix@gmail.com>
2024-04-26 17:47:04 +0300

    react-redux güncellemeleri

--- react-redux/src/modals/EditTodo.jsx
+++ react-redux/src/modals/EditTodo.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react"
 
-function EditTodo({data,close}) {
+function EditTodo({ data, close }) {
     const [todo, setTodo] = useState(data.title)
-    const [done,setDone] = useState(data.done)
+    const [done, setDone] = useState(data.done)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
     return (
-       <form onSubmit={handleSubmit}>
-           <input type="text" value={todo} onChange={e => setTodo(e.target.value)} /> <br/>
-           <label>
-            <input type="checkbox" checked={done} onChange={e => setDone(e.target.checked)} />
-           </label>
-       </form> 
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={todo} onChange={e => setTodo(e.target.value)} /> <br />
+            <label>
+                <input type="checkbox" checked={done} onChange={e => setDone(e.target.checked)} />
+                Tamamlandı
+            </label>
+        </form>
     )
 }
 
